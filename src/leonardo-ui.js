725756711453d2d5ed2c7c7fb7f13f072b24526d
Rickard Svensson <rsv@qlik.com>
2018-07-11 09:42:07 +0200

    Update build-time dependencies

--- src/leonardo-ui.js
+++ src/leonardo-ui.js
@@ -6,13 +6,6 @@ import tooltip from './components/tooltip/tooltip';
 // Import LESS
 import './leonardo-ui.less';
 
-// Copy files
-/* eslint-disable import/no-extraneous-dependencies, import/imports-first, import/no-unresolved */
-import 'file?name=colors.less!./colors.less';
-import 'file?name=variables.less!./variables.less';
-import 'file?name=lui-icons.ttf!./resources/lui-icons.ttf';
-import 'file?name=lui-icons.woff!./resources/lui-icons.woff';
-
 export {
   dialog,
   popover,
